c6a8b6ac32a982d00f0b5f4cdf205770f3453964
Samuel Fernandes <samuelfesant@gmail.com>
2016-09-17 11:03:33 -0300

    Feito o redir do logout para o login

--- baixei/logout.php
+++ baixei/logout.php
@@ -3,5 +3,6 @@ session_start();//INICIANDO A SESSÃO
 session_destroy();//DESTRUINDO A SESSÃO
 session_unset();//LIMPANDO AS VARIÁVEIS GLOBAIS
 
-echo "<script>alert('Você se deslogou com sucesso!');;top.location.href='login.php';</script>";
+header('Location: login.php');
+exit();
 ?>
